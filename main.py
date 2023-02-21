7104f2ed99ae473bdfb46f2a88448c7f45e05baa
xenia <troshkinaksenia@icloud.com>
2023-02-21 19:24:40 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+
+from data.jobs import Jobs
+from data import db_session
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+def main():
+    db_session.global_init("db/mars.db")
+    app.run()
+
+
+@app.route("/")
+def index():
+    db_sess = db_session.create_session()
+    jobs = db_sess.query(Jobs)
+    return render_template("index.html", jobs=jobs)
+
+
+if __name__ == '__main__':
+    main()
